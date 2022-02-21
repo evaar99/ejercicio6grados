514b5c938596d5c814a35a01c95497c2030c09cb
evaren <evaren@ubuntu>
2022-02-21 17:26:13 +0100

    Se han añadido 2 clases (Numeros y Dinero). Además se han creado 2 métodos para cada clase.

---
+++ src/ejercicio6grados/Dinero.java
@@ -0,0 +1,21 @@
+package ejercicio6grados;
+
+/**
+ *
+ * @author evaren
+ */
+public class Dinero {
+    private int doll;
+    private int eur;
+    
+    private double euro2dollar(int num){
+        //Este método está por implementar.
+        return (num * 1.13);
+    }
+    
+    private double dollar2euro(int num){
+        //Este método está por implementar.
+        return (num * 0.88);
+    }   
+    
+}
---
+++ src/ejercicio6grados/Numeros.java
@@ -0,0 +1,26 @@
+package ejercicio6grados;
+
+/**
+ *
+ * @author evaren
+ */
+public class Numeros {
+    private int dec;
+    private int rom;
+    
+    //Constructor
+    public Numeros(int x, int y) {
+        dec = x;
+        rom = y;
+    }
+    
+    public String roman2dec(int num){
+        //Este métdo está por implementar.
+        return "XII";
+    }
+    
+    public int dec2roman(int num){
+        //Este métdo está por implementar.
+        return 10;
+    }
+}
